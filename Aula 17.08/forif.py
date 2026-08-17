06021c14c0b3be72386aaf509c53145a0b32a7e8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-17 14:40:06 -0300

    teste

--- Aula 17.08/forif.py
+++ Aula 17.08/forif.py
@@ -1,2 +1,16 @@
 # Combinando FOR com IF
 
+vendas = [1200, 1500, 300, 800, 900, 100, 450, 3000, 700, 5000]
+meta = 1200
+
+# Quais valores bateram a meta
+for venda in vendas:
+    if venda >= meta:
+        print(venda)
+
+# Quantos valores bateram a meta
+metas_batidas = 0
+for venda in vendas:
+    if venda >= meta:
+        metas_batidas += 1
+        print(metas_batidas)
